527b37fb38a5ae85a58d9708fcaa4b581331ef7b
RollandHenri <rolland.h.dev@gmail.com>
2023-03-10 12:04:21 +0100

    Evidence
    En cours

--- script/index.js
+++ script/index.js
@@ -1,10 +1,6 @@
 /**** Variables ****/
 const navbar = document.querySelector("nav");
 const logo = document.querySelector(".nav-logo");
-const aboutImgMid = document.querySelector(".about-middle-left");
-const aboutTextMid = document.querySelector(".about-middle-right");
-const aboutImgBot = document.querySelector(".about-bottom-right");
-const aboutTextBot = document.querySelector(".about-bottom-left");
 
 /**** Anim Dom ****/
 window.addEventListener("scroll", () => {
@@ -12,31 +8,5 @@ window.addEventListener("scroll", () => {
     navbar.classList.add("navBar-js");
   } else if (scrollY < 80) {
     navbar.classList = "";
-  }
-});
-
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  if (scrollY < 750) {
-    aboutImgMid.style.transform = "translateX(-100%)";
-    aboutImgMid.style.opacity = 0;
-    aboutTextMid.style.transform = "translateX(100%)";
-    aboutTextMid.style.opacity = 0;
-  } else if (scrollY > 750) {
-    aboutImgMid.style.transform = "translateX(0)";
-    aboutImgMid.style.opacity = 1;
-    aboutTextMid.style.transform = "translateX(0)";
-    aboutTextMid.style.opacity = 1;
-  }
-  if (scrollY < 1430) {
-    aboutTextBot.style.transform = "translateX(-100%)";
-    aboutTextBot.style.opacity = 0;
-    aboutImgBot.style.transform = "translateX(100%)";
-    aboutImgBot.style.opacity = 0;
-  } else if (scrollY > 1430) {
-    aboutTextBot.style.transform = "translateX(0)";
-    aboutTextBot.style.opacity = 1;
-    aboutImgBot.style.transform = "translateX(0)";
-    aboutImgBot.style.opacity = 1;
   }
 });
